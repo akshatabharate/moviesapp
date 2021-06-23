e89f8c2a7837bb3b97079debb8974629b859bdd7
akshata <bharate.akshata@gmail.com>
2021-06-23 09:43:22 +0530

    display  data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
 
     def index
-
+       @movies = Movie.all
     end
+    def show
+        @movie =Movie.find(params[:id])
+    end
+
+   private def movie_params
+      param.requere(:movie).permit(:title, :rating)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   root 'movies#index'
+  get  'show' => 'movies#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
