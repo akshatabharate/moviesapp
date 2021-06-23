436e9f0685bc8fb5cc494350254ff4c33974b1a8
akshata <bharate.akshata@gmail.com>
2021-06-23 09:20:29 +0530

    created model

--- db/migrate/20210623034933_create_movies.rb
+++ db/migrate/20210623034933_create_movies.rb
@@ -1,6 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.integer :rating
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_033631) do
+ActiveRecord::Schema.define(version: 2021_06_23_034933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
