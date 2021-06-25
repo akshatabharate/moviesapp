45bac24ebd1df4e5d8a41bfbb064cb2734978669
akshata <bharate.akshata@gmail.com>
2021-06-25 09:22:25 +0530

    added month in string formate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,11 +3,11 @@ class MoviesController < ApplicationController
     def index
        @movies = Movie.all
     end
-    def show
-        @movie =Movie.find(params[:id])
-    end
+    # def show
+    #     @movie =Movie.find(params[:id])
+    # end
 
-   private def movie_params
-      param.requere(:movie).permit(:title, :rating, :releasedon, :summery)
-   end
+#    private def movie_params
+#       param.requere(:movie).permit(:title, :rating, :releasedon, :summery)
+#    end
 end
